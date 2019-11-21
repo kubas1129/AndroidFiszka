c5927cb5ce529517b5b86c44d15f50a530527b42
Adrian Opiela <adrian.opiela11@gmail.com>
2019-11-21 21:07:28 +0100

    create buttons and input fields

--- app/src/main/java/com/example/androidfiszka/MainActivity.java
+++ app/src/main/java/com/example/androidfiszka/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.androidfiszka;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button createCategoryButton;
+    private Button createFiszka;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createCategoryButton = (Button) findViewById(R.id.createCategoryButton);
+        createCategoryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCreateCategory();
+            }
+        });
+        createCategoryButton = (Button) findViewById(R.id.createFiszkaButton);
+        createCategoryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCreateFiszka();
+            }
+        });
     }
+
+    public void openCreateCategory() {
+        Intent intent = new Intent(this, CreateCategoryActivity.class);
+        startActivity(intent);
+    }
+
+    public void openCreateFiszka() {
+        Intent intent = new Intent(this, CreateFiszkaActivity.class);
+        startActivity(intent);
+    }
+
 }

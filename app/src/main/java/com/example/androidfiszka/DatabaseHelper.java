3967de7dabe1aaa8064124abd199190446bc7520
Kuba <jakub.palka@vistorm.pl>
2019-11-21 22:13:07 +0100

    DBHelper and mainactivity

---
+++ app/src/main/java/com/example/androidfiszka/DatabaseHelper.java
@@ -0,0 +1,79 @@
+package com.example.androidfiszka;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "fiszkaDataBase.db";
+    public static final String CATEGORY_TABLE_NAME = "fiszkaCategories";
+    public static final String FISZKA_TABLE_NAME = "fiszka";
+
+    public DatabaseHelper(Context context){super(context,DATABASE_NAME,null,1);}
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createCategories = "CREATE TABLE " + CATEGORY_TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, DESCRIPTION TEXT)";
+        db.execSQL(createCategories);
+        String createFiszka = "CREATE TABLE " + FISZKA_TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, CATEGORY_NAME TEXT, QUESTION TEXT, ANSWER TEXT)";
+        db.execSQL(createFiszka);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + CATEGORY_TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + FISZKA_TABLE_NAME);
+    }
+
+    public boolean AddCategory(String categoryName, String categoryDesc){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("NAME",categoryName);
+        contentValues.put("DESCRIPTION", categoryDesc);
+
+        long result =  db.insert(CATEGORY_TABLE_NAME,null,contentValues);
+        if(result == -1){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+
+    public Cursor GetAllCategories(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor data = db.rawQuery("SELECT *  FROM "  + CATEGORY_TABLE_NAME,null);
+        return data;
+    }
+
+
+    public boolean AddFiszka(String categoryName, String question, String answer){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("CATEGORY_NAME",categoryName);
+        contentValues.put("QUESTION",question);
+        contentValues.put("ANSWER", answer);
+
+        long result = db.insert(FISZKA_TABLE_NAME,null,contentValues);
+        if(result == -1)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+
+    public Cursor GetAllFiszkaByCategory(String categoryName){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor data = db.rawQuery("SELECT * FROM "  + FISZKA_TABLE_NAME + " WHERE CATEGORY_NAME = " + categoryName,null);
+        return data;
+    }
+
+}
--- app/src/main/java/com/example/androidfiszka/MainActivity.java
+++ app/src/main/java/com/example/androidfiszka/MainActivity.java
@@ -2,13 +2,61 @@ package com.example.androidfiszka;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG  ="MainActivity";
+
+    DatabaseHelper myDB;
+    Button btnAddCategory, btnAddFiszka;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAddCategory = (Button) findViewById(R.id.createCategoryButton);
+        btnAddFiszka = (Button) findViewById(R.id.createFiszkaButton);
+        listView = (ListView) findViewById(R.id.categoryList);
+        myDB = new DatabaseHelper(this);
+
+        btnAddCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(MainActivity.this,"CLICK CATEGORIES",Toast.LENGTH_LONG).show();
+
+                ArrayList<String> cateogies = new ArrayList<>();
+                Cursor data = myDB.GetAllCategories();
+
+                if(data.getCount() ==  0){
+                    Toast.makeText(MainActivity.this,"EMPTY DB",Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this,"NOT EMPTY DB",Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+        btnAddFiszka.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"CLICK FISZKA",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
     }
 }

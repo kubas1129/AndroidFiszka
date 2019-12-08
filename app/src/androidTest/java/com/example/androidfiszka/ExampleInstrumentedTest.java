b40d23177724bbf46be36fe642a30dda6010749d
pawel17291729 <58186342+pawel17291729@users.noreply.github.com>
2019-12-08 16:47:35 +0100

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/androidfiszka/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/androidfiszka/ExampleInstrumentedTest.java
@@ -4,10 +4,15 @@ import android.content.Context;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -17,6 +22,11 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<CreateCategoryActivity> mCreateCategoryActivity = new ActivityTestRule<CreateCategoryActivity>(CreateCategoryActivity.class);
+
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
@@ -24,4 +34,14 @@ public class ExampleInstrumentedTest {
 
         assertEquals("com.example.androidfiszka", appContext.getPackageName());
     }
+
+    @Test
+    public void testCreateCategory(){
+
+        onView(withId(R.id.categoryName)).perform(typeText("Jezyk angielski"));
+
+        onView(withId(R.id.categoryDesciption)).perform(typeText("zestaw fiszek do nauki jezyka angielskiego"));
+
+    }
+
 }
